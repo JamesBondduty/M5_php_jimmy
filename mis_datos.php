f137363838e12226a0368e5f1eaa42165c1b3981
JamesBondduty <jamesbondduty@gmail.com>
2026-01-29 22:57:49 +0000

    Primeros pasos

---
+++ mis_datos.php
@@ -0,0 +1,10 @@
+<?php
+    $nombres = "Jaime";
+    $apellidos = "Garía Rodríguez";
+    $correo = "jamesbd@gmail.com";
+    $cargo = "encargado de tesorería";
+    $domicilio = "Rosedal, Coyoacán";
+
+    $nombre_completo = $nombres . " " . $apellidos;
+?>
+
